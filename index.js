b076d6b4bd31b6eb58e00e4b2230ee8d5d1259ad
RISSCoding <farisfebrian0602@gmail.com>
2023-10-19 09:43:23 +0700

    inisiaisasi

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ const express = require('express')
 const app = express()
 const port = 3333
 
-app.get('/',(req,res) =>{
-    res.send("Hello ini express")
+app.get('/pesanmenu',(req,res) =>{
+    res.send("pesan apa?")
+})
+
+app.put('/pesanmenu',(req,res) =>{
+    res.send("teh manis ada")
+})
+
+app.post('/pesanmenu',(req,res) =>{
+    res.send("nasgr ada")
+})
+app.delete('/pesanmenu',(req,res) =>{
+    res.send("mie ada")
 })
 
 app.listen(port,() => console.log(`Server running on port ${port}`))
