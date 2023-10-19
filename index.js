fb76991d76c2af4845e0a344dd1a1ea231cc62e4
RISSCoding <farisfebrian0602@gmail.com>
2023-10-19 09:48:08 +0700

    afs

--- index.js
+++ index.js
@@ -3,19 +3,27 @@ const express = require('express')
 const app = express()
 const port = 3333
 
-app.get('/pesanmenu',(req,res) =>{
-    res.send("pesan apa?")
-})
+app.get("/pesanmenu", (req, res) => {
+    res.json({
+        message: "ini pesan menu",
+    });
+});
 
-app.put('/pesanmenu',(req,res) =>{
-    res.send("teh manis ada")
-})
+app.put("/pesanmenu", (req, res) => {
+    res.json({
+        message: "mau pesan makanan atau minuman?",
+    });
+});
 
-app.post('/pesanmenu',(req,res) =>{
-    res.send("nasgr ada")
-})
-app.delete('/pesanmenu',(req,res) =>{
-    res.send("mie ada")
-})
+app.post("/pesanmenu", (req, res) => {
+    res.json({
+        message: "ada banyak makanan",
+    });
+});
+app.delete("/pesanmenu", (req, res) => {
+    res.json({
+        message: "ada banyak minuman",
+    });
+});
 
 app.listen(port,() => console.log(`Server running on port ${port}`))
